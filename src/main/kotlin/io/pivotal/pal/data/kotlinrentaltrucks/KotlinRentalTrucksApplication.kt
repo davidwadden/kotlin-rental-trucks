08364c94c601bdf98c30590c52e13f9da9e10d00
David Wadden <dwadden@pivotal.io>
2018-04-22 22:20:34 -0500

    Imports project from Spring Initializr

---
+++ src/main/kotlin/io/pivotal/pal/data/kotlinrentaltrucks/KotlinRentalTrucksApplication.kt
@@ -0,0 +1,11 @@
+package io.pivotal.pal.data.kotlinrentaltrucks
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinRentalTrucksApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinRentalTrucksApplication>(*args)
+}
