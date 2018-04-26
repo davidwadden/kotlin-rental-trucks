af4cef89e701e3532a6accb1ada469c9be8a648e
David Wadden <dwadden@pivotal.io>
2018-04-25 23:12:10 -0500

    Defines reservation entity within JPA with tests

--- src/main/kotlin/io/pivotal/pal/data/rentaltruck/reservation/domain/Reservation.kt
+++ src/main/kotlin/io/pivotal/pal/data/rentaltruck/reservation/domain/Reservation.kt
@@ -1,12 +1,29 @@
 package io.pivotal.pal.data.rentaltruck.reservation.domain
 
 import java.time.LocalDate
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.Id
+import javax.persistence.Table
 
+@Entity
+@Table(name = "reservation", schema = "reservation")
 data class Reservation(
+
+        @Id
+        @Column(name = "confirmation_number", nullable = false, unique = true)
         val confirmationNumber: String,
+
+        @Column(name = "status", nullable = false)
         var status: String,
+
+        @Column(name = "start_date", nullable = false)
         val startDate: LocalDate,
+
+        @Column(name = "end_date", nullable = false)
         val endDate: LocalDate,
+
+        @Column(name = "customer_name", nullable = false)
         val customerName: String
 ) {
 
