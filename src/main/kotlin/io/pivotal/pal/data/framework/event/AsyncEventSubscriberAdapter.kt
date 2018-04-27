4c9d4b113f05ddcdf69da8f1575deea46e76ad90
David Wadden <dwadden@pivotal.io>
2018-04-26 22:30:46 -0500

    Handles errors from async + sync events
    Signed-off-by: Tim Dalsing <tdalsing@pivotal.io>

--- src/main/kotlin/io/pivotal/pal/data/framework/event/AsyncEventSubscriberAdapter.kt
+++ src/main/kotlin/io/pivotal/pal/data/framework/event/AsyncEventSubscriberAdapter.kt
@@ -3,8 +3,11 @@ package io.pivotal.pal.data.framework.event
 import org.slf4j.LoggerFactory
 import java.util.concurrent.LinkedBlockingQueue
 
-
-class AsyncEventSubscriberAdapter<T>(eventName: String, private val handler: AsyncEventHandler<T>) : AsyncEventChannel(eventName) {
+class AsyncEventSubscriberAdapter<T>(
+        eventName: String,
+        private val handler: AsyncEventHandler<T>,
+        private val errorHandler: AsyncEventHandler<T>? = null
+) : AsyncEventChannel(eventName) {
     private val queue = LinkedBlockingQueue<T>()
 
     init {
@@ -17,13 +20,19 @@ class AsyncEventSubscriberAdapter<T>(eventName: String, private val handler: Asy
         override fun run() {
 
             while (true) {
+                var data: T? = null
+
                 try {
-                    val data = queue.take()
+                    data = queue.take()
 
                     logger.debug("calling event handler={}: data={}")
                     handler.onEvent(data)
                 } catch (x: Exception) {
                     logger.error("exception thrown in event processor thread", x)
+
+                    if (errorHandler != null && data != null) {
+                        errorHandler.onEvent(data)
+                    }
                 }
 
             }
--- src/test/kotlin/io/pivotal/pal/data/framework/event/AsyncEventTest.kt
+++ src/test/kotlin/io/pivotal/pal/data/framework/event/AsyncEventTest.kt
@@ -7,21 +7,25 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 
+
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 internal class AsyncEventTest {
 
     private var data: String? = null
-
-    private val publisher: DefaultAsyncEventPublisher<String> = DefaultAsyncEventPublisher(EVENT_NAME)
-    private val subscriber: AsyncEventSubscriberAdapter<String> = AsyncEventSubscriberAdapter(EVENT_NAME, Handler())
+    private var errorData: String? = null
 
     @BeforeEach
     fun setUp() {
         data = null
+        errorData = null
     }
 
+
     @Test
-    fun `should publish an event asynchronously to the subscriber`() {
+    fun `publishes an event asynchronously to the subscriber`() {
+        val publisher = DefaultAsyncEventPublisher<String>(EVENT_NAME)
+        val subscriber = AsyncEventSubscriberAdapter(EVENT_NAME, Handler())
+
         val someData = "some-data"
         publisher.publish(someData)
 
@@ -30,10 +34,56 @@ internal class AsyncEventTest {
                 .untilAsserted { assertThat(data).isEqualTo(someData) }
     }
 
+    @Test
+    fun `invokes the error handler upon error`() {
+        val publisher = DefaultAsyncEventPublisher<String>(EVENT_NAME)
+        val subscriber = AsyncEventSubscriberAdapter(EVENT_NAME, ExceptionThrowingHandler(), ErrorHandler())
+
+        val someData = "some-data"
+        publisher.publish(someData)
+
+        await()
+                .atMost(ONE_SECOND)
+                .untilAsserted {
+                    assertThat(errorData).isEqualTo(someData)
+                    assertThat(data).isNull()
+                }
+    }
+
+    @Test
+    fun `handles an error gracefully without an error handler`() {
+        val publisher = DefaultAsyncEventPublisher<String>(EVENT_NAME)
+        val subscriber = AsyncEventSubscriberAdapter(EVENT_NAME, ExceptionThrowingHandler())
+
+        val someData = "some-data"
+        publisher.publish(someData)
+
+        await()
+                .atMost(ONE_SECOND)
+                .untilAsserted {
+                    assertThat(errorData).isNull()
+                    assertThat(data).isNull()
+                }
+    }
+
     private inner class Handler : AsyncEventHandler<String> {
 
         override fun onEvent(data: String) {
             this@AsyncEventTest.data = data
+        }
+    }
+
+    private inner class ErrorHandler : AsyncEventHandler<String> {
+
+        override fun onEvent(data: String) {
+            this@AsyncEventTest.errorData = data
+        }
+    }
+
+    private inner class ExceptionThrowingHandler : AsyncEventHandler<String> {
+
+        override fun onEvent(data: String) {
+            throw IllegalArgumentException("data = $data")
         }
     }
 
