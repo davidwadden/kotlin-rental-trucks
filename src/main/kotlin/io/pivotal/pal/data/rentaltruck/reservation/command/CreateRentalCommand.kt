32bb914355e592ecc886c815e9ba87abd9d66c3e
David Wadden <dwadden@pivotal.io>
2018-05-06 14:40:42 -0500

    Refactors create rental handler to use Mono.zip

--- src/main/kotlin/io/pivotal/pal/data/rentaltruck/reservation/command/CreateRentalCommand.kt
+++ src/main/kotlin/io/pivotal/pal/data/rentaltruck/reservation/command/CreateRentalCommand.kt
@@ -2,10 +2,12 @@ package io.pivotal.pal.data.rentaltruck.reservation.command
 
 import io.pivotal.pal.data.rentaltruck.reservation.domain.Rental
 import io.pivotal.pal.data.rentaltruck.reservation.domain.RentalStatus
+import io.pivotal.pal.data.rentaltruck.reservation.domain.Reservation
 import io.pivotal.pal.data.rentaltruck.reservation.domain.ReservationRepository
 import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
 import reactor.core.publisher.Mono
+import reactor.util.function.Tuple2
 import java.net.URI
 
 internal data class CreateRentalCommandDto(
@@ -24,32 +26,25 @@ class CreateRentalCommandHandler(
                 // deserialize the commandDto off the request
                 .bodyToMono(CreateRentalCommandDto::class.java)
                 // look up associated reservation by confirmation number
-                .map {
+                .flatMap<Tuple2<CreateRentalCommandDto, Reservation>> {
                     val res = reservationRepository.findByConfirmationNumber(it.confirmationNumber)
-                    Pair(it, res)
-                }
-                // business invariant: prevent rentals with invalid confirmation numbers
-                .map { (commandDto, res) ->
-                    if (res == null) {
-                        throw IllegalArgumentException()
-                    }
-                    Pair(commandDto, res)
+                    Mono.zip(Mono.justOrEmpty(it), Mono.justOrEmpty(res))
                 }
                 // build rental (child) and chain on with reservation (parent)
-                .map { (commandDto, reservation) ->
+                .map { tuple ->
                     val rental = Rental(
-                            rentalId = commandDto.rentalId,
-                            confirmationNumber = commandDto.confirmationNumber,
+                            rentalId = tuple.t1.rentalId,
+                            confirmationNumber = tuple.t1.confirmationNumber,
                             status = RentalStatus.PENDING,
-                            reservation = reservation,
-                            truckId = commandDto.truckId,
-                            pickUpDate = reservation.pickUpDate,
-                            scheduledDropOffDate = reservation.dropOffDate,
+                            reservation = tuple.t2,
+                            truckId = tuple.t1.truckId,
+                            pickUpDate = tuple.t2.pickUpDate,
+                            scheduledDropOffDate = tuple.t2.dropOffDate,
                             dropOffDate = null,
-                            customerName = reservation.customerName,
+                            customerName = tuple.t2.customerName,
                             dropOffMileage = null
                     )
-                    Pair(rental, reservation)
+                    Pair(rental, tuple.t2)
                 }
                 // create immutable copy of reservation with 1:1 rental added
                 .map { (rental, reservation) ->
