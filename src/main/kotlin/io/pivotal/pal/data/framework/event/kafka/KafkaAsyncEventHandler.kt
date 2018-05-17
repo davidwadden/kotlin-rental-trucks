d0dea00c263a9417ae041f676d76c20c70040777
David Wadden <dwadden@pivotal.io>
2018-05-17 00:08:33 -0500

    Renames Kafka event subscriber to event handler

--- src/main/kotlin/io/pivotal/pal/data/framework/event/kafka/KafkaAsyncEventHandler.kt
+++ src/main/kotlin/io/pivotal/pal/data/framework/event/kafka/KafkaAsyncEventHandler.kt
@@ -3,7 +3,7 @@ package io.pivotal.pal.data.framework.event.kafka
 import io.pivotal.pal.data.framework.event.AsyncEventHandler
 import org.springframework.kafka.core.KafkaTemplate
 
-class KafkaAsyncEventSubscriber<T>(private val eventName: String, private val template: KafkaTemplate<Any, T>) : AsyncEventHandler<T> {
+class KafkaAsyncEventHandler<T>(private val eventName: String, private val template: KafkaTemplate<Any, T>) : AsyncEventHandler<T> {
 
     override fun onEvent(event: T) {
         template.send(eventName, event)
